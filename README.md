ec2f474e48acfc33bbc939bf957f76f06004d5d5
JonahWhaler <jonah_mytzuchi@outlook.com>
2025-01-03 09:29:56 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # audio-to-text-pipeline
-POC project based on llm-agent-toolkit to convert audio to text.
+This project showcases steps to transform audio file to transcript.
+
+# Dependencies
+* **ffmpeg**: v4.4.2 # Ubuntu
+* **llm-agent-toolkit[transcriber]**: 0.0.24
+
+# Why use llm-agent-toolkit?
+OpenAI's `whisper-1` API accepts file uploads up to 25MB. `OpenAITranscriber` streamline the transcribing experience by integrating the ability to split large audio file into acceptable chunks.
+User may utilize `AudioHelper` to generate audio chunks.
+
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+# Dependencies
 import os
 from dotenv import load_dotenv
-from llm_agent_toolkit import TranscriptionConfig, transcriber
+from llm_agent_toolkit.transcriber import TranscriptionConfig
+from llm_agent_toolkit.transcriber.open_ai import OpenAITranscriber
 
 
 if __name__ == "__main__":
     load_dotenv()
-
+    # Input-Output
     AUDIO_FILES = [
         # "./audio/part_0.mp3",
         # "./audio/part_1.mp3",
@@ -13,10 +15,11 @@ if __name__ == "__main__":
     ]
     OUTPUT_DIRECTORY = "./output"
 
+    # Execution
     whisper_config = TranscriptionConfig(
         name="whisper-1", temperature=0.3, response_format="text"
     )
-    agent = transcriber.open_ai.OpenAITranscriber(config=whisper_config)
+    agent = OpenAITranscriber(config=whisper_config)
 
     for audio_file in AUDIO_FILES:
         basename = os.path.basename(audio_file)
@@ -29,3 +32,19 @@ if __name__ == "__main__":
             filepath=audio_file,
             tmp_directory=export_directory,
         )
+
+    # View Output
+    folders = os.listdir(OUTPUT_DIRECTORY)
+    for folder in folders:
+        files = os.listdir(os.path.join(OUTPUT_DIRECTORY, folder))
+        for file in files:
+            ext = os.path.splitext(file)[1]
+            if ext != ".md":
+                continue
+
+            filepath = os.path.join(OUTPUT_DIRECTORY, folder, file)
+            print(f"Filepath: {filepath}")
+            with open(filepath, "r", encoding="utf-8") as f:
+                line = f.read()
+                print(f">>>> {line[:80]}")
+            print("\n")
